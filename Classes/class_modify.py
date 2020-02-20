0024e941439a113d7ff64b8012acd50fc1cbe586
Souvik Ghosh_tech <42302494+SouvikSSG@users.noreply.github.com>
2020-02-20 18:41:45 +0530

    Update class_modify.py

--- Classes/class_modify.py
+++ Classes/class_modify.py
@@ -20,6 +20,9 @@ class Names:
 
     @classmethod
     def class_var(cls):  # this function is reference to the class
+        # this function is reference to the class
+        # can be accessed by 'Names.class_var() and
+        # 'Names().class_var() both.
         return cls.counter
 
 
@@ -44,3 +47,8 @@ print(Names.class_var())  # as 'class_var()' is reference to the class,
 # we can write like this.
 # 'class_var()' will be independent on whatever
 # the amount of object is created.
+
+## attributes can't be accessed using class like 'class.attribute'
+## but, it can accessed using object like 'class().attribute'
+## where, object=class().
+
