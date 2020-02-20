7eb11ef229de57e23a6baf1e06b91c88d9d7ef34
Souvik Ghosh_tech <42302494+SouvikSSG@users.noreply.github.com>
2020-02-20 19:15:34 +0530

    Update class_modify.py

--- Classes/class_modify.py
+++ Classes/class_modify.py
@@ -1,6 +1,5 @@
 # the following codes are easy to do with class
 
-
 class Names:
     counter = 0  # it is the class variable
 
@@ -26,8 +25,17 @@ class Names:
 
 name_obj_1 = Names('Souvik', 'Ghosh')  # it will take arguments in __init_
 # now it will run what is defined in __init__()
-print(dir(Names))
+
+print(Names.counter)
+
+print(Names.class_var())  # as 'class_var()' is reference to the class,
+# we can write like this.
+
 name_obj_2 = Names('Guido', 'Van')  # it will run __init__() again
+
+print(Names.counter)
+
+print(Names.class_var())
 
 print(name_obj_1)  # it will return all the dunder method used in class
 # for this object 'name_obj_1
@@ -35,7 +43,7 @@ print(Names.printing_name(name_obj_1))  # accessing with class
 
 print(name_obj_2)  # returns all dunder for this object 'name_obj_2'
 
-print (Names.printing_name(name_obj_2))
+print(Names.printing_name(name_obj_2))
 
 print(name_obj_1.printing_name())  # we are accessing function defined
 # inside the class
@@ -45,12 +53,7 @@ print(name_obj_1.fullname)  # we want to print a specified value of the
 # attribute of this class
 print(name_obj_2.fullname)
 
-print(Names.class_var())  # as 'class_var()' is reference to the class,
-# we can write like this.
-# 'class_var()' will be independent on whatever
-# the amount of object is created.
-
 ## attributes can't be accessed using class like 'class.attribute'
 ## but, it can accessed using object like 'class().attribute'
 ## where, object=class().
-
+print(dir(Names))
