1104a3d1aa76c202bd1ae3e51f5a9fff5a78d3ea
Souvik Ghosh_tech <42302494+SouvikSSG@users.noreply.github.com>
2020-02-22 02:20:55 +0530

    Updated func_

--- Functions/geeks_dict.py
+++ Functions/geeks_dict.py
@@ -1,3 +1,14 @@
+"""
+Copyright (c) 2020, Souvik Ghosh.
+
+Distributed under the terms of the MIT License.
+
+The full license is in the file LICENSE, distributed with this software.
+
+Created on Feb 20, 2020
+
+@author
+"""
 string = "11-13,12-14,13-15"
 
 Dict = dict((x, y) for x, y in (element.split('-') for element in string.split(',')))
