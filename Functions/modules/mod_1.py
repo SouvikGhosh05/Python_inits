8060e0b2bc6869e33ed814b0872e23e75dec057c
Souvik Ghosh_tech <42302494+SouvikSSG@users.noreply.github.com>
2020-03-09 02:22:28 +0530

    updated main_mod

--- Functions/modules/mod_1.py
+++ Functions/modules/mod_1.py
@@ -1,3 +1,15 @@
+"""
+Copyright (c) 2020, Souvik Ghosh.
+
+Distributed under the terms of the MIT License.
+
+The full license is in the file LICENSE, distributed with this software.
+
+Created on Feb 25, 2020
+
+@author
+"""
+
 def func_list():
     x = input("\n input the items of list: ")
     return list(x)
@@ -8,7 +20,7 @@ if __name__ == "__main__":    ## only runs in this environment.
     number_of_lists = int(input("enter the number of lists to input: "))
     while i <= (number_of_lists - 1):
         updated_list_1 = func_list()  ## returns a list with the input entries.
-        print(updated_list_1)        ## prints the list.
-        for j in updated_list_1:     ## iterates over the items of the list.
-            print(j, end=' ')        ## prints individual items of the list.
+        print(updated_list_1)         ## prints the list.
+        for j in updated_list_1:      ## iterates over the items of the list.
+            print(j, end=' ')         ## prints individual items of the list.
         i += 1
