4c2e3d10a617f71f4f6fc8faf4d437991cb8e794
Souvik Ghosh_tech <42302494+SouvikSSG@users.noreply.github.com>
2020-03-09 02:19:20 +0530

    Added a dec_

--- Functions/decorators/python_decorators.py
+++ Functions/decorators/python_decorators.py
@@ -1,15 +1,27 @@
+"""
+Copyright (c) 2020, Souvik Ghosh.
+
+Distributed under the terms of the MIT License.
+
+The full license is in the file LICENSE, distributed with this software.
+
+Created on Mar 09, 2020
+
+@author
+"""
+
 def dec_1(func):
     def wrapper_1(*args, **kwargs):  # this 'wrapper_1()' function allows the arguments and
-        # keyword argument to be pass in 'func()' function.
+                                     # keyword argument to be pass in 'func()' function.
 
         print("I'm before decorator.")
 
         rv_1 = func(*args, **kwargs)  # as 'func()' is inside a decorator function, this function represents
-        # several functions which is to be passed inside this decorator.
+                                      # several functions which is to be passed inside this decorator.
         print("I'm after decorator.")
         return rv_1  # here 'return' is used to return something from the 'wrapper_1()' function. If we don't
-        # mention 'return' of 'wrapper_1(), then the inner-decorated function won't return anything even if any
-        # defined function returns anything using this decorator.
+                     # mention 'return' of 'wrapper_1(), then the inner-decorated function won't return anything even if any
+                     # defined function returns anything using this decorator.
 
     return wrapper_1  # this returns the function object of 'wrapper()' function.
 
@@ -45,7 +57,7 @@ def sum_func(*args):
     return [i for i in args]
 
 
-u_1 = func_1("I'm Souvik.", stat='Python')  # here, this 'func_1()' is being called as 'wrapper_1()' function
+u_1 = func_1("I'm Souvik.", stat='Python')   # here, this 'func_1()' is being called as 'wrapper_1()' function
                                              # defined in the decorator 'dec_1'.
 
 @dec_2
@@ -60,7 +72,7 @@ def out_dec(*args):  # this function is totally independent on any of these deco
 
 
 u_2 = func_1("Hello, What's up!", stat='Bro!')  # here, this 'func_1()' is being called as 'wrapper_2()' function
-# defined in the decorator 'dec_1'.
+                                                # defined in the decorator 'dec_1'.
 v = func_2()
 w = sum_func(5, 10, 20)
 
@@ -70,13 +82,13 @@ print(w)  # it is similar as that of 'u' defined above.
 print(u_1)  # 'u_1' is the returned value of the 'func_1()' function passed in 'dec_1' decorator.
 print(u_2)  # 'u_2' is the returned value of the 'func_1()' function passed in 'dec_2' decorator.
 
-o_d = out_dec()  # this  'out_dec()' function is called independently here.
-print(o_d)  # here, 'o_d' is the returned value of the 'out_dec()' function independently.
-x = dec_1(out_dec)  # here, 'out_dec' is being treated as 'func' inside 'dec_1' decorator.
-y = dec_2(out_dec)  # here, 'out_dec' is being treated as 'func' inside 'dec_2' decorator.
-print(x)  # here, 'x' is 'wrapper_1' function-object from 'dec_1' decorator.
-print(y)  # here, 'y' is 'wrapper_2' function-object from 'dec_2' decorator.
+o_d = out_dec()           # this  'out_dec()' function is called independently here.
+print(o_d)                # here, 'o_d' is the returned value of the 'out_dec()' function independently.
+x = dec_1(out_dec)        # here, 'out_dec' is being treated as 'func' inside 'dec_1' decorator.
+y = dec_2(out_dec)        # here, 'out_dec' is being treated as 'func' inside 'dec_2' decorator.
+print(x)                  # here, 'x' is 'wrapper_1' function-object from 'dec_1' decorator.
+print(y)                  # here, 'y' is 'wrapper_2' function-object from 'dec_2' decorator.
 x_ret = x(100, 200, 300)  # here, 'x()' function is 'wrapper_1()' function.
 y_ret = y(200, 400, 600)  # here, 'y()' function is 'wrapper_2()' function.
-print(x_ret)  # here, 'x_ret' is the returned value of the 'out_dec()' function passed in 'dec_1' decorator.
-print(y_ret)  # here, 'y_ret' is the returned value of the 'out_dec()' function passed in 'dec_2' decorator.
+print(x_ret)              # here, 'x_ret' is the returned value of the 'out_dec()' function passed in 'dec_1' decorator.
+print(y_ret)              # here, 'y_ret' is the returned value of the 'out_dec()' function passed in 'dec_2' decorator.
