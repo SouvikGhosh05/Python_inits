907393431cd5ee2db53fa4f91d59480a97291fbe
Souvik Ghosh_tech <42302494+SouvikSSG@users.noreply.github.com>
2020-03-09 02:20:56 +0530

    mod_ updated

--- Functions/modules/importing.py
+++ Functions/modules/importing.py
@@ -1,3 +1,15 @@
+"""
+Copyright (c) 2020, Souvik Ghosh.
+
+Distributed under the terms of the MIT License.
+
+The full license is in the file LICENSE, distributed with this software.
+
+Created on Feb 25, 2020
+
+@author
+"""
+
 import mod_1 as md1 # we're importing 'mod_1' file named as 'md1'
 
 updated_put = md1.func_list()  # the function itself returns the list
@@ -10,3 +22,4 @@ from mod_1 import *          # 'import *' means it imports all the functions
 updated_put = func_list()
 for i in updated_put:
     print(i, end=' ')
+    
